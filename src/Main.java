496da19b7b3ade04d7dc535c56c516eaf9ceb2e4
vasylec <cozma.vasyle@gmail.com>
2025-10-07 13:40:25 +0300

    saave

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -9,7 +12,13 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/game.fxml"));
+        Parent root = loader.load();
 
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("FXML Example");
+        stage.setScene(scene);
+        stage.show();
         stage.setWidth(500);
         stage.setHeight(500);
 
