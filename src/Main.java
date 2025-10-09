6c06bf996d355f308c30da9214d1847c43cbfefa
vasylec <cozma.vasyle@gmail.com>
2025-10-09 21:04:23 +0300

    save

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -58,6 +60,13 @@ public class Main extends Application{
         Main.fullscreen = false;
 
         initializeSound();
+
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+
+        double width = screenBounds.getWidth();
+        double height = screenBounds.getHeight();
+
+        System.out.println("Rezolutia ecranului: " + width + " x " + height);
         
         
         
